9fc4cbba99c17a5f4d99e9ed9c1ec9fa905e217e
Eddie Naff <eddie@eddienaff.dev>
2020-09-01 08:48:13 -0700

    Embellish Set methods

--- src/set.js
+++ src/set.js
@@ -27,7 +27,7 @@ function MySet() {
   };
 
   // this method will remove an element from a Set
-  this.remove = function (element) {
+  this.delete = function (element) {
     if (this.has(element)) {
       const index = collection.indexOf(element);
       collection.splice(index, 1);
@@ -40,7 +40,7 @@ function MySet() {
     return collection.length;
   };
 
-  // this method will return the union of two sets
+  // This method will return the union of two sets
   this.union = function (otherSet) {
     const unionSet = new Set();
     const firstSet = this.values();
@@ -53,8 +53,66 @@ function MySet() {
     });
     return unionSet;
   };
+
+  // This method will return teh difference of two sets
+  // as a new set.
+  this.intersection = function (otherSet) {
+    const intersectionSet = new MySet();
+    const firstSet = this.values();
+    firstSet.forEach(function (val) {
+      if (otherSet.has(val)) {
+        intersectionSet.add(val);
+      }
+    });
+    return intersectionSet;
+  };
+
+  // This method will return the difference of two
+  // sets as a new Set.
+  this.difference = function (otherSet) {
+    const differenceSet = new MySet();
+    const firstSet = this.values();
+    firstSet.forEach(function (val) {
+      if (!otherSet.has(val)) {
+        differenceSet.add(val);
+      }
+    });
+    return differenceSet;
+  };
+
+  // This method will test if the set is a subset of a
+  // different Set.
+  this.subset = function (otherSet) {
+    const firstSet = this.values();
+    return firstSet.every(function (value) {
+      return otherSet.has(value);
+    });
+  };
 }
 
-const makeSet = new MySet();
+// ============
+// MANUAL TESTING
 
-console.log(makeSet.has("blerg"));
+const setA = new MySet();
+const setB = new MySet();
+setA.add("a");
+setB.add("b");
+setB.add("c");
+setB.add("a");
+setB.add("d");
+
+console.log(setA.subset(setB));
+console.log(setA.intersection(setB).values());
+console.log(setB.difference(setA).values());
+
+const setC = new Set();
+const setD = new Set();
+setC.add("a");
+setD.add("b");
+setD.add("c");
+setD.add("a");
+setD.add("d");
+console.log(setD.values());
+setD.delete("a");
+console.log(setD.has("a"));
+console.log(setD.add("d"));
