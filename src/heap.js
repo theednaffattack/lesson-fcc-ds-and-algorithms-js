0df343d5ca65fe3bf52db0c2c42e6aeb9290cb82
Eddie Naff <eddie@eddienaff.dev>
2020-09-04 13:58:33 -0700

    Add Heap data structure

--- src/heap.js
+++ src/heap.js
@@ -1,3 +1,5 @@
+const imgcat = require("imgcat");
+
 /* HEAP */
 
 /* 
@@ -5,4 +7,40 @@
 https://youtu.be/t2CEgPsws3U?list=PLDn22JhUbNqaf_suvX_NYIdPkFOnFIicR&t=5251
  */
 
-function Heap() {}
+// left child: i * 2
+// right child: i * 2 + 1
+// parent: i / 2
+
+let MinHeap = function () {
+  let heap = [null];
+
+  this.insert = function (num) {
+    heap.push(num);
+    if (heap.length > 2) {
+      let idx = heap.length - 1;
+      while (heap[idx] < heap[Math.floor(idx / 2)]) {
+        if (idx >= 1) {
+          [heap[Math.floor(idx / 2)], heap[idx]] = [
+            heap[idx],
+            heap[Math.floor(idx / 2)],
+          ];
+
+          if (Math.floor(idx / 2) > 1) {
+            idx = Math.floor(idx / 2);
+          } else {
+            break;
+          }
+        }
+      }
+    }
+  };
+
+  this.remove = function () {
+    // do some stuff
+  };
+};
+
+imgcat(
+  "https://cdn-media-1.freecodecamp.org/images/1*Lu5E1YaakS3JFcCqOsiniw.png",
+  { log: true }
+);
