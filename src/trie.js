049cfcac0762b8ff0ee579ef9bb0e76872407aa1
Eddie Naff <eddie@eddienaff.dev>
2020-09-04 13:46:31 -0700

    Add methods to Trie

--- src/trie.js
+++ src/trie.js
@@ -1,3 +1,5 @@
+const imgcat = require("imgcat");
+
 /* Trie */
 
 /* 
@@ -30,13 +32,75 @@ let Trie = function () {
       node.setEnd();
       return;
     } else if (!node.keys.has(input[0])) {
+      // above: if the Map keys DOES NOT contain a letter
+      //  matching the first letter of 'input'...
+
+      // below: set a key in the Map to the first
+      // letter of 'input'. A new Node will be the
+      // value in the map.
       node.keys.set(input[0], new Node());
       return this.add(input.substr(1), node.keys.get(input[0]));
     } else {
       return this.add(input.substr(1), node.keys.get(input[0]));
     }
   };
+
+  this.isWord = function (word) {
+    let node = this.root;
+    while (word.length > 1) {
+      if (!node.keys.has(word[0])) {
+        return false;
+      } else {
+        node = node.keys.get(word[0]);
+        word = word.substr(1);
+      }
+    }
+    return node.keys.has(word) && node.keys.get(word).isEnd() ? true : false;
+  };
+
+  this.print = function () {
+    let words = new Array();
+    let search = function (node, string) {
+      if (node.keys.size != 0) {
+        for (const letter of node.keys.keys()) {
+          search(node.keys.get(letter), string.concat(letter));
+        }
+        if (node.isEnd) {
+          words.push(string);
+        }
+      } else {
+        string.length > 0 ? words.push(string) : undefined;
+        return;
+      }
+    };
+    search(this.root, new String());
+    return words.length > 0 ? words : null;
+  };
 };
 
-// NOTE: ti = Trie Instance
-// const ti = Trie();
+// MANUAL TEST
+
+const ti = new Trie();
+
+ti.add("ball");
+ti.add("bat");
+ti.add("doll");
+ti.add("dork");
+ti.add("do");
+ti.add("dorm");
+ti.add("send");
+ti.add("sense");
+console.log(ti.isWord("doll"));
+console.log(ti.isWord("dor"));
+console.log(ti.isWord("dorf"));
+console.log(ti.print());
+
+// print image from file
+imgcat("src/image/trie.png")
+  .then((image) => {
+    console.log(image);
+  })
+  .catch((e) => {
+    console.log(e.name);
+    console.log(e);
+  });
